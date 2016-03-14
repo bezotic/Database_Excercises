ef5bde84cd993eeb826d7f4f7eda267cf4446b55
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-03-14 11:39:36 -0500

    added albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,16 @@
- USE codeup_test_db;
+	USE codeup_test_db;
+
+	DROP TABLE IF EXISTS albums;
+
+	CREATE TABLE albums (
+		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		artist varchar(200) NOT NULL,
+		name varchar(200) NOT NULL,
+		release_date date NOT NULL,
+		sales FLOAT NOT NULL, 
+		genre varchar(200) NOT NULL,
+		PRIMARY KEY (id)
+
+
+
+	);
