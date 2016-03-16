7d4267abee8c98b497214fa7fde5724f40329412
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-03-16 09:43:16 -0500

    modified order_by_exercises added birth_date on WHERE.

---
+++ limit_exercises.sql
@@ -0,0 +1,21 @@
+SELECT distinct  last_name
+FROM employees
+WHERE last_name LIKE '%z%'
+GROUP BY last_name desc
+limit 10;
+
+
+SELECT birth_date, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date desc
+limit 5;
+
+
+SELECT birth_date, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date asc, hire_date desc
+limit 5 OFFSET 45
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -104,7 +104,7 @@ SELECT birth_date, first_name, last_name
 FROM employees
 WHERE birth_date LIKE '%-12-25'
 AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-ORDER BY hire_date desc;
+ORDER BY birth_date asc, hire_date desc;
 
 
 
